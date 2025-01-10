c78ed92424f35a602a08efb5e25a77a834f90aec
Nathan-Hutton <nathan.d.hutton@gmail.com>
2025-01-10 11:10:17 -0700

    Got red window to render

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,59 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
+
+void resize_window(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main()
 {
-    std::cout << "Hello, world!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // These 2 lines are just so I can set the window size to be the size of the monitor in glfwCreateWindow
+    GLFWmonitor* monitor { glfwGetPrimaryMonitor() };
+    const GLFWvidmode* mode { glfwGetVideoMode(monitor) };
+
+    GLFWwindow* window { glfwCreateWindow(mode->width, mode->height, "One-Particle System", NULL, NULL) };
+    if (window == NULL)
+    {
+        std::cout << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Place the window at the top-left corner of the screen (might not be necessary)
+    //glfwSetWindowPos(window, 0, 0);
+    
+    glViewport(0, 0, mode->width, mode->height);
+    glfwSetFramebufferSizeCallback(window, resize_window);
+
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Set background color to red
+
+    while (!glfwWindowShouldClose(window)) 
+    {
+        processInput(window);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void resize_window(GLFWwindow* window, int width, int height)
+{
+    (void)window; // This just gets rid of the unused parameter warning
+    glViewport(0, 0, width, height);
 }
